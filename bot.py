0aff58a5e5896454746288dc0af1320d1dc59582
cvam0000 <cvam0000@gmail.com>
2019-01-22 21:35:15 +0530

    nothing

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import telepot
 from pprint import pprint
+from telepot .loop import MessageLoop
 bot=telepot.Bot("538862630:AAF8LlV3qBSOKLDgbx-I1ZdTZvdtEdtQcVU")
 pprint(bot.getMe())
 responce=bot.getUpdates()
 pprint(responce)
+
+
+#linux_puran -1001169337107
+bot.sendMessage(499134543, 'Hey! bro ')

6f4728033709d0e13a5dca575b4b85b647f38b14
cvam0000 <cvam0000@gmail.com>
2019-01-27 22:09:01 +0530

    update

--- bot.py
+++ bot.py
@@ -47,4 +47,4 @@ for x in range(9):
 
 
 
-bot.sendMessage(-1001169337107, 'https://opensource.com/article/19/1/productivity-tool-tint2' ,parse_mode= 'Markdown')
+bot.sendMessage(-1001169337107, 'https://www.2daygeek.com/how-to-understand-and-identify-file-types-in-linux/' ,parse_mode= 'Markdown')

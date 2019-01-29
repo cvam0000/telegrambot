9efb6cb72bc78828c382462b828843bf8b8697e1
cvam0000 <cvam0000@gmail.com>
2019-01-29 14:46:16 +0530

    update

--- bot.py
+++ bot.py
@@ -1,50 +1,19 @@
 import telepot
-from pprint import pprint
-from telepot .loop import MessageLoop
-import requests
-from requests import *
-import json
-import time 
+bot = telepot.Bot('438212244:AAGQ1VhzLHowzxUTFiT6GTirIVxjL3PO79I')
+bot.sendMessage(499134543, '2nd feb, 11AM at 91SpringBoard, Jhandewalan. Be on time everyone')
 
-from hn import HN #hacker news 
-
-bot=telepot.Bot("538862630:AAF8LlV3qBSOKLDgbx-I1ZdTZvdtEdtQcVU")
-pprint(bot.getMe())
-#responce1=bot.getUpdates()
-#pprint(responce1)
-
-
-#linux_puran -1001169337107
-
-
-
-
-#https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=API_KEY  techcrunch_API
-
-url='https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e353c11878134fc28d81faf7eff3b941'
-
-resp = requests.get(url).json()
-#if resp.status_code != 200:
-    # This means something went wrong.
-    #print('hello')
-    #raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-
-#response = requests.get(url)
-
-for x in range(9):
-    title=resp['articles'][x]['title']
-    description=resp['articles'][x]['title']
-    url=resp['articles'][x]['url']
-    source = resp['articles'][x]['source']['name']
-    output=  "***"+source +"***\n"+description+"\n"+url
-    #bot.sendMessage(499134543, output ,parse_mode= 'Markdown')
-    #time.sleep(100)
-
-
-#print(resp['articles'][0]['title'])
-
-#print(resp)
-v='https://github.com/STEllAR-GROUP/hpx/wiki/GSoC-2019-Project-Ideas'
-
-
-bot.sendMessage(-1001169337107, v )
+bot.sendMessage(499134543, text )
+        #ajx
+bot.sendMessage(680139252, text)
+        #mason
+bot.sendMessage(660463343, text)
+        #ayan
+bot.sendMessage(400670226, text)
+        #alok
+bot.sendMessage(271397625, text)
+        #dunken
+bot.sendMessage(399195869, text)
+        #math
+bot.sendMessage(407034913, text)
+        #bit_bang
+bot.sendMessage(-1001163644641,text)
---
+++ linux_puran_bot.py
@@ -0,0 +1,50 @@
+import telepot
+from pprint import pprint
+from telepot .loop import MessageLoop
+import requests
+from requests import *
+import json
+import time 
+
+from hn import HN #hacker news 
+
+bot=telepot.Bot("538862630:AAF8LlV3qBSOKLDgbx-I1ZdTZvdtEdtQcVU")
+pprint(bot.getMe())
+#responce1=bot.getUpdates()
+#pprint(responce1)
+
+
+#linux_puran -1001169337107
+
+
+
+
+#https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=API_KEY  techcrunch_API
+
+url='https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e353c11878134fc28d81faf7eff3b941'
+
+resp = requests.get(url).json()
+#if resp.status_code != 200:
+    # This means something went wrong.
+    #print('hello')
+    #raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+#response = requests.get(url)
+
+for x in range(9):
+    title=resp['articles'][x]['title']
+    description=resp['articles'][x]['title']
+    url=resp['articles'][x]['url']
+    source = resp['articles'][x]['source']['name']
+    output=  "***"+source +"***\n"+description+"\n"+url
+    #bot.sendMessage(499134543, output ,parse_mode= 'Markdown')
+    #time.sleep(100)
+
+
+#print(resp['articles'][0]['title'])
+
+#print(resp)
+v='https://github.com/STEllAR-GROUP/hpx/wiki/GSoC-2019-Project-Ideas'
+
+
+bot.sendMessage(-1001169337107, v )
